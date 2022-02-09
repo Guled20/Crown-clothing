1e14819b5b20f04dda9a7c26e5874cb1ff4fe3ab
Guled Mohamed <hooyomcn1234@gmail.com>
2022-02-09 19:20:36 +0300

    integrated firebase into our appilcation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import SignInAndSignUp from './pages/signIn-and-signUp/signIn-and-signUp.component';
 import Header from './components/header/header.component';
+import {auth} from './firebase/firebase.utils'
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage}/>
-      </Switch>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser:null,
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUp}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+    apiKey: "AIzaSyAl5utSBT11UC0LdrR40EYr5FiaRYuWbKY",
+    authDomain: "crwn-db-2e1e4.firebaseapp.com",
+    projectId: "crwn-db-2e1e4",
+    storageBucket: "crwn-db-2e1e4.appspot.com",
+    messagingSenderId: "706823676208",
+    appId: "1:706823676208:web:d73afd973a6ec59a16e8f5"
+  };
+
+  
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
